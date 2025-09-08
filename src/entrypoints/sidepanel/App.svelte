ce3a658e5fc804551cad0c62a9cb4c8fbee22b5b
Luckwut <roihan2007@gmail.com>
2025-09-08 21:01:24 +0700

    RuleItem for sidepanel

--- src/entrypoints/sidepanel/App.svelte
+++ src/entrypoints/sidepanel/App.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
   import wxtLogo from "@/assets/wxt.svg";
-  import {
-    Pencil,
-    Settings,
-  } from "@lucide/svelte";
+  import RuleItem from "@/lib/sidepanel/components/RuleItem.svelte";
+  import { Settings } from "@lucide/svelte";
+
+  const test = [
+    {
+      id: "id-1",
+      name: "Youtube",
+      enabled: true,
+      dailyLimit: 4,
+      unlockCount: 1,
+    },
+    {
+      id: "id-2",
+      name: "Youtube",
+      enabled: false,
+      dailyLimit: 4,
+      unlockCount: 0,
+    },
+  ];
 </script>
 
 <main class="flex flex-col bg-base-300 h-screen">
   <header
-    class="flex justify-between items-center p-2 border-b border-b-base-100"
+    class="flex justify-between items-center py-2 px-3 border-b border-b-base-100"
   >
     <div class="flex items-center gap-2">
       <img src={wxtLogo} alt="Terminate Distraction Logo" class="w-6" />
@@ -19,40 +34,17 @@
     </button>
   </header>
 
-  <div class="flex flex-col flex-1 gap-2 p-2 min-h-0 overflow-y-auto topography-pattern scrollbar-hidden">
-    {#each { length: 12 }}
-      <div class="flex flex-col gap-2 bg-base-100 shadow rounded-xl p-2">
-        <div class="flex flex-1 gap-2 items-center text-sm">
-          <span class="flex flex-1 gap-2 min-w-0">
-            <span class="font-semibold text-green-600">0/6</span>
-            <span class="truncate flex-1">Youtube Distraction</span>
-          </span>
-          <button class="btn btn-xs btn-soft rounded-full hover:btn-primary w-16">
-            Unlock
-          </button>
-        </div>
-        <span class="border-t border-base-content/10"></span>
-        <div class="flex flex-1 justify-between items-center text-xs">
-          <span class="flex gap-1">
-            <span class="font-bold">Status:</span>
-            <span>Active</span>
-          </span>
-          <span class="flex gap-2 items-center">
-            <span class="text-base-content/35">0 days streak</span>
-            <button class="btn btn-xs btn-soft hover:btn-primary btn-circle">
-              <Pencil size={12} />
-            </button>
-          </span>
-        </div>
-      </div>
+  <div
+    class="flex flex-col flex-1 items-center gap-2 p-2 min-h-0 overflow-y-auto topography-pattern scrollbar-hidden"
+  >
+    {#each test as t}
+      <RuleItem {...t} />
     {/each}
   </div>
 
-  <div
-    class="flex justify-center items-center p-2 border-t border-t-base-100"
-  >
+  <div class="flex justify-center items-center p-2 border-t border-t-base-100">
     <button class="btn btn-sm btn-soft btn-primary btn-wide rounded-full">
-      <span class="text-primary-content">Add Site Group</span>
+      <span class="text-primary-content">Add New Rule</span>
     </button>
   </div>
 </main>
---
+++ src/lib/sidepanel/components/RuleItem.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+    import { Settings2 } from "@lucide/svelte";
+
+    interface Props {
+        id: string;
+        name: string;
+        enabled: boolean;
+        dailyLimit: number;
+        unlockCount: number;
+    }
+
+    let p: Props = $props();
+
+    const displayButton = $derived.by(() => {
+        if (!p.enabled) {
+            return buttonDisabled;
+        }
+
+        if (p.unlockCount >= p.dailyLimit) {
+            return buttonLocked;
+        }
+
+        return buttonUnlock;
+    });
+
+    const displayDailyLimitClass = $derived.by(() => {
+        if (!p.enabled) {
+            return "text-base-content/35";
+        }
+
+        if (p.unlockCount + 1 === p.dailyLimit) {
+            return "text-yellow-600";
+        }
+
+        if (p.unlockCount === p.dailyLimit) {
+            return "text-red-600";
+        }
+
+        return "text-green-600";
+    });
+</script>
+
+{#snippet buttonUnlock()}
+    <button class="btn btn-xs btn-soft hover:btn-primary rounded-full px-4">
+        Unlock
+    </button>
+{/snippet}
+
+{#snippet buttonUnlockedTimer()}
+    <div
+        class="btn btn-xs btn-soft btn-primary rounded-full px-4 pointer-events-none tabular-nums"
+    >
+        Unlocked 10:00
+    </div>
+{/snippet}
+
+{#snippet buttonCooldownTimer()}
+    <div
+        class="btn btn-xs btn-soft btn-warning rounded-full px-4 pointer-events-none tabular-nums"
+    >
+        Cooldown 10:00
+    </div>
+{/snippet}
+
+{#snippet buttonLocked()}
+    <button
+        class="btn btn-xs btn-soft btn-error rounded-full px-4 pointer-events-none"
+    >
+        Locked
+    </button>
+{/snippet}
+
+{#snippet buttonDisabled()}
+    <button class="btn btn-xs rounded-full px-4" disabled>Disabled</button>
+{/snippet}
+
+<div
+    class="flex flex-col gap-2 bg-base-100 shadow rounded-xl p-2 max-w-lg w-full"
+>
+    <div class="flex flex-1 gap-2 items-center text-sm">
+        <span class="flex flex-1 gap-2 min-w-0">
+            <span class="font-semibold {displayDailyLimitClass}">
+                {p.unlockCount}/{p.dailyLimit}
+            </span>
+            <span class="truncate flex-1">
+                {p.name}
+            </span>
+        </span>
+        {@render displayButton()}
+    </div>
+    <span class="border-t border-base-content/10"></span>
+    <div class="flex flex-1 justify-between items-center text-xs">
+        <span class="flex gap-1 items-center">
+            <span class="text-base-content/35">AREA TODO</span>
+        </span>
+        <span class="flex gap-2 items-center">
+            <span class="text-base-content/35">0 days streak</span>
+            <button class="btn btn-xs btn-soft hover:btn-primary btn-circle">
+                <Settings2 size={12} />
+            </button>
+        </span>
+    </div>
+</div>

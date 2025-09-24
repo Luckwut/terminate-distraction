f5b5468d7a16c7449d8041fe3b50ea036184bfb0
Luckwut <roihan2007@gmail.com>
2025-09-24 16:40:04 +0700

    better preset rules by adding support for m.youtube.com and more accurate selectors

--- src/lib/data/rules/presetData.ts
+++ src/lib/data/rules/presetData.ts
@@ -15,25 +15,49 @@ export const presetData: Rule[] = [
     sites: [
       {
         id: crypto.randomUUID(),
-        siteUrl: "*.youtube.com/",
-        actions: [  
+        siteUrl: "www.youtube.com/",
+        actions: [
           {
             id: crypto.randomUUID(),
             type: "HIDE_ELEMENT",
             label: "Homepage feed",
-            selector: "#contents"
+            selector: "div#contents"
           }
         ]
       },
       {
         id: crypto.randomUUID(),
-        siteUrl: "*.youtube.com/watch*",
+        siteUrl: "m.youtube.com/",
+        actions: [
+          {
+            id: crypto.randomUUID(),
+            type: "HIDE_ELEMENT",
+            label: "Homepage feed",
+            selector: "ytm-app#app > div.page-container > ytm-browse.YtmBrowseHost > ytm-single-column-browse-results-renderer.modern-tabs > div > div.tab-content > ytm-tab-renderer > ytm-rich-grid-renderer > div.rich-grid-renderer-contents.rich-grid-top-margin:nth-of-type(2)"
+          }
+        ]
+      },
+      {
+        id: crypto.randomUUID(),
+        siteUrl: "www.youtube.com/watch*",
         actions: [
           {
             id: crypto.randomUUID(),
             type: "HIDE_ELEMENT",
             label: "Related video",
-            selector: "#secondary"
+            selector: "div#items"
+          },
+        ]
+      },
+      {
+        id: crypto.randomUUID(),
+        siteUrl: "m.youtube.com/watch*",
+        actions: [
+          {
+            id: crypto.randomUUID(),
+            type: "HIDE_ELEMENT",
+            label: "Related video",
+            selector: "ytm-app#app > div.page-container:nth-of-type(2) > ytm-watch.ambient-topbar > div.watch-below-the-player:nth-of-type(2) > ytm-single-column-watch-next-results-renderer.watch-content.full-bleed-wn-thumbs > div.related-items-container"
           },
         ]
       },
@@ -53,25 +77,98 @@ export const presetData: Rule[] = [
     sites: [
       {
         id: crypto.randomUUID(),
-        siteUrl: "*.youtube.com/",
+        siteUrl: "www.youtube.com/",
         actions: [
           {
             id: crypto.randomUUID(),
             type: "HIDE_ELEMENT",
             label: "Homepage shorts section",
-            selector: "#content > ytd-rich-shelf-renderer"
+            selector: "div#contents > ytd-rich-section-renderer.style-scope.ytd-rich-grid-renderer"
           },
         ]
       },
+      
       {
         id: crypto.randomUUID(),
-        siteUrl: "*.youtube.com/watch*",
+        siteUrl: "www.youtube.com/watch*",
         actions: [
           {
             id: crypto.randomUUID(),
             type: "HIDE_ELEMENT",
             label: "Related video shorts section",
-            selector: "#contents > ytd-reel-shelf-renderer"
+            selector: "div#contents > ytd-reel-shelf-renderer.style-scope.ytd-item-section-renderer"
+          },
+        ]
+      },
+      {
+        id: crypto.randomUUID(),
+        siteUrl: "www.youtube.com/results*",
+        actions: [
+          {
+            id: crypto.randomUUID(),
+            type: "HIDE_ELEMENT",
+            label: "Search shorts",
+            selector: "div#contents > grid-shelf-view-model.ytGridShelfViewModelHost.ytd-item-section-renderer.ytGridShelfViewModelHostHasBottomButton"
+          },
+        ]
+      },
+      {
+        id: crypto.randomUUID(),
+        siteUrl: "www.youtube.com/@*/shorts*",
+        actions: [
+          {
+            id: crypto.randomUUID(),
+            type: "HIDE_ELEMENT",
+            label: "Account shorts",
+            selector: "div#contents"
+          },
+        ]
+      },
+      {
+        id: crypto.randomUUID(),
+        siteUrl: "m.youtube.com/",
+        actions: [
+          {
+            id: crypto.randomUUID(),
+            type: "HIDE_ELEMENT",
+            label: "Homepage shorts section",
+            selector: "ytm-tab-renderer > ytm-rich-grid-renderer > div.rich-grid-renderer-contents > ytm-rich-section-renderer:not(:has(ytm-backstage-post-renderer)) > div.rich-section-content"
+          },
+        ]
+      },
+      {
+        id: crypto.randomUUID(),
+        siteUrl: "m.youtube.com/watch*",
+        actions: [
+          {
+            id: crypto.randomUUID(),
+            type: "HIDE_ELEMENT",
+            label: "Related video shorts section",
+            selector: "ytm-reel-shelf-renderer.item"
+          },
+        ]
+      },
+      {
+        id: crypto.randomUUID(),
+        siteUrl: "m.youtube.com/results*",
+        actions: [
+          {
+            id: crypto.randomUUID(),
+            type: "HIDE_ELEMENT",
+            label: "Search shorts",
+            selector: "ytm-app#app > div.page-container > ytm-search > ytm-section-list-renderer.no-bottom-separator > lazy-list > ytm-item-section-renderer > lazy-list > ytm-reel-shelf-renderer.item > div.reel-shelf-items"
+          },
+        ]
+      },
+      {
+        id: crypto.randomUUID(),
+        siteUrl: "m.youtube.com/@*/shorts*",
+        actions: [
+          {
+            id: crypto.randomUUID(),
+            type: "HIDE_ELEMENT",
+            label: "Account shorts",
+            selector: "ytm-tab-renderer > ytm-rich-grid-renderer.is-shorts.is-shorts-gallery > div.rich-grid-renderer-contents:nth-of-type(2)"
           },
         ]
       },
